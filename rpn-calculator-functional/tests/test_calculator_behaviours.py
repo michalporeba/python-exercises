842687e9542e002520c287f103c453c07fb2d3a2
Michał Poręba <michalporeba@gmail.com>
2024-03-09 19:27:47 +0000

    extra tests

--- rpn-calculator-functional/tests/test_calculator_behaviours.py
+++ rpn-calculator-functional/tests/test_calculator_behaviours.py
@@ -5,11 +5,14 @@ def test_simple_addition():
     assert 3 == calculate("1 2 +")
     assert 4 == calculate("2 2 +")
     assert 5 == calculate("4 1 +")
+    assert 3 == calculate("-3 6 +")
 
 def test_simple_multiplication():
     assert 6 == calculate("2 3 *")
     assert 12 == calculate("3 4 *")
     assert 21 == calculate("3 7 *")
+    assert -10 == calculate("2 -5 *")
 
 def test_simple_subtraction():
     assert 1 == calculate("4 3 -")
+    assert -1 == calculate("3 4 -")
