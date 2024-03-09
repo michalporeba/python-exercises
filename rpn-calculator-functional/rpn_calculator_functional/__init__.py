3bc8954b5fbd443d57ca9f27d400c524a60a3877
Michał Poręba <michalporeba@gmail.com>
2024-03-09 19:16:18 +0000

    refactoring

--- rpn-calculator-functional/rpn_calculator_functional/__init__.py
+++ rpn-calculator-functional/rpn_calculator_functional/__init__.py
@@ -1,24 +1,24 @@
 def calculate(equation):
-  symbols = parse_symbols(equation)
-  stack = []
+    symbols = parse_symbols(equation)
+    stack = []
 
-  while len(symbols) > 0:
-    print("----")
-    print(symbols)
-    print(stack)
-    if callable(symbols[0]):
-      return symbols[0](stack.pop(), stack.pop())
-    else:
-      stack += [symbols.pop(0)]
+    while len(symbols) > 0:
+        if callable(symbols[0]):
+            return symbols[0](stack.pop(), stack.pop())
+        else:
+            stack += [symbols.pop(0)]
+
 
 def parse_symbols(equation):
-  return [_parse_symbol(s) for s in equation.split()]
+    return [_parse_symbol(s) for s in equation.split()]
+
 
 def _parse_symbol(symbol):
-  try:
-    return int(symbol)
-  except:
-    return _add
-  
+    try:
+        return int(symbol)
+    except:
+        return _add
+
+
 def _add(a, b):
-  return a + b
+    return a + b
