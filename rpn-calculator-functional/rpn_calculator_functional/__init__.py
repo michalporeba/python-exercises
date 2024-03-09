2e5c663a0f98918f03b2775a81fd31ba2bef7846
Michał Poręba <michalporeba@gmail.com>
2024-03-09 18:59:11 +0000

    variable input results in different results

--- rpn-calculator-functional/rpn_calculator_functional/__init__.py
+++ rpn-calculator-functional/rpn_calculator_functional/__init__.py
@@ -1,2 +1,5 @@
 def calculate(equation):
-  return 3
+  symbols = equation.split()
+  if symbols[0] == "1":
+    return 3
+  return 4
--- rpn-calculator-functional/tests/test_calculator_behaviours.py
+++ rpn-calculator-functional/tests/test_calculator_behaviours.py
@@ -2,3 +2,4 @@ from rpn_calculator_functional import *
 
 def test_test():
   assert 3 == calculate("1 2 +")
+  assert 4 == calculate("2 2 +")
